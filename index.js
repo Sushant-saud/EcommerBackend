617e647af680c7e82d7101a783ebd6b2c7c85b2c
Sushant-saud <sushantsaud9804@gmail.com>
2022-12-12 17:46:39 +0530

    d

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+let PORT=process.env.PORT;
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require("dotenv");
@@ -18,14 +19,16 @@ mongoose.connect(process.env.MONGO_URL,{
     console.log("Database Disconnected");
 })
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api/user',userRoute)
-app.use('/api/cart',cartRoute)
+app.use('/api/user',userRoute);
+app.use('/api/cart',cartRoute);
 app.use('/api/product',productRoute);
 app.use('/api/order',orderRoute);
+
 app.use('/api/auth',authRoute);
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Benkend server is running");
+app.listen(PORT || 6000,() => {
+    console.log("Benkend server is running ");
 })
 
 
